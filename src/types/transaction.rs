5ba79a0d334b8a01a28cf788d9ad9ad1dc96ae75
Hugo Lin <sylin.hugo@gmail.com>
2022-01-21 23:30:24 -0600

    Finished second part of Warmup1 question

--- src/types/transaction.rs
+++ src/types/transaction.rs
@@ -1,28 +1,78 @@
-use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+extern crate ring;
+
 use rand::Rng;
+use ring::digest::{self, Context, Digest, SHA256};
+use ring::signature::{
+    self, Ed25519KeyPair, EdDSAParameters, KeyPair, Signature, VerificationAlgorithm,
+};
+use serde::{Deserialize, Serialize};
+
+use super::address::Address;
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Transaction {
+    sender: Address,
+    receiver: Address,
+    value: u8,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
-pub struct SignedTransaction {
-}
+pub struct SignedTransaction {}
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    // Because key.sign() only accept &[u8], so we need to figure out how to convert t into &[u8] type
+
+    // Convert to Vec<u8> first
+    let serial_res = bincode::serialize(t).unwrap();
+
+    // Convert to &Vec<u8>
+    // let u8_serial_res = &serial_res;
+    let u8_serial_res_2: &[u8] = &serial_res;
+    // let hash_txt = digest::digest(&digest::SHA256, serial_res.as_ref()).as_ref();
+    let signature: Signature = key.sign(u8_serial_res_2);
+    signature
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &[u8], signature: &[u8]) -> bool {
-    unimplemented!()
+    // Because in UnparsedPublicKey, the verify need to accept &[u8] as parameter
+    // so we need to convert it
+    let serial_res = bincode::serialize(t).unwrap();
+    let u8_serial_res_2: &[u8] = &serial_res;
+
+    // Verify the signature of the message using the public key. Normally the
+    // verifier of the message would parse the inputs to this code out of the
+    // protocol message(s) sent by the signer.
+    let public_key_ = signature::UnparsedPublicKey::new(&signature::ED25519, public_key.as_ref());
+    let res = public_key_
+        .verify(u8_serial_res_2, signature.as_ref())
+        .is_ok();
+    res
 }
 
 #[cfg(any(test, test_utilities))]
 pub fn generate_random_transaction() -> Transaction {
-    unimplemented!()
+    use std::convert::TryInto;
+
+    let mut rng = rand::thread_rng();
+    let mut sender = Vec::<u8>::with_capacity(20);
+    let mut receiver = Vec::<u8>::with_capacity(20);
+
+    for _ in 0..20 {
+        sender.push(rng.gen());
+        receiver.push(rng.gen());
+    }
+
+    let sender_addr: [u8; 20] = sender.try_into().unwrap();
+    let receiver_addr: [u8; 20] = receiver.try_into().unwrap();
+
+    let transc = Transaction {
+        sender: Address::new(sender_addr),
+        receiver: Address::new(receiver_addr),
+        value: rng.gen(),
+    };
+    transc
 }
 
 // DO NOT CHANGE THIS COMMENT, IT IS FOR AUTOGRADER. BEFORE TEST
@@ -32,7 +82,6 @@ mod tests {
     use super::*;
     use crate::types::key_pair;
     use ring::signature::KeyPair;
-
 
     #[test]
     fn sign_verify() {
