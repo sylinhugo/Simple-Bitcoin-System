cdd581be105a5051f1e4626cd15f61e3076aacb8
Hugo <sylin.hugo@gmail.com>
2022-02-06 01:24:06 -0600

    Finished part one of midterm1; still not run test case yet

--- src/types/block.rs
+++ src/types/block.rs
@@ -1,27 +1,104 @@
-use serde::{Serialize, Deserialize};
-use crate::types::hash::{H256, Hashable};
+use super::transaction::SignedTransaction;
+use crate::types::hash::{Hashable, H256};
+use serde::{Deserialize, Serialize};
 
+// According to midterm1, add Block, BlockHeader and BlockContent
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
+    pub header: BlockHeader,
+    pub content: BlockContent,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BlockHeader {
+    pub parent: H256,
+    pub nonce: u32,
+    pub difficulty: H256,
+    pub timestamp: u128,
+    pub merkle_root: H256,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BlockContent {
+    // Fuck, we didn't implement SignedTranscation in warmup1...
+    pub content: Vec<SignedTransaction>,
+}
+
+// According to midterm1, imple Hashable for Block
 impl Hashable for Block {
     fn hash(&self) -> H256 {
-        unimplemented!()
+        self.header.hash()
+    }
+}
+
+// According to midterm1, imple Hashable for BlockHeader
+impl Hashable for BlockHeader {
+    fn hash(&self) -> H256 {
+        let serial_res = bincode::serialize(&self).unwrap();
+        let res = ring::digest::digest(&ring::digest::SHA256, &serial_res).into();
+        res
     }
 }
 
 impl Block {
+    // Add this in midterm1, never use it though
     pub fn get_parent(&self) -> H256 {
-        unimplemented!()
+        let res = self.header.parent;
+        res
     }
 
+    // Add this in midterm1, never use it though
     pub fn get_difficulty(&self) -> H256 {
-        unimplemented!()
+        let res = self.header.difficulty;
+        res
     }
 }
 
 #[cfg(any(test, test_utilities))]
 pub fn generate_random_block(parent: &H256) -> Block {
-    unimplemented!()
+    use crate::types::{
+        merkle::MerkleTree,
+        transaction::{generate_random_transaction, SignedTransaction, Transaction},
+    };
+    use rand::Rng;
+    use std::{convert::TryInto, time::SystemTime};
+
+    let mut rng = rand::thread_rng();
+    // Generate random nonce
+    let block_nonce: u32 = rng.gen();
+
+    // Generate random difficulty, and remember H256 is [u8;32]
+    let mut difficulty = Vec::<u8>::with_capacity(32);
+    for _ in 0..32 {
+        difficulty.push(rng.gen());
+    }
+    let tmp_difficulty: [u8; 32] = difficulty.try_into().unwrap();
+    let block_difficulty: H256 = tmp_difficulty.into();
+
+    // Assign current system timestamp to block
+    let block_timestamp: u128 = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    // Generate fake transcation for testing
+    let fake_content: Vec<SignedTransaction> = Vec::<SignedTransaction>::new();
+
+    // Generate merkle tree with fake content
+    let merkle_tree: MerkleTree = MerkleTree::new(&fake_content);
+
+    let block_header = BlockHeader {
+        parent: *parent,
+        nonce: block_nonce,
+        difficulty: block_difficulty,
+        timestamp: block_timestamp,
+        merkle_root: merkle_tree.root(),
+    };
+    let block_content = BlockContent {
+        content: fake_content,
+    };
+    let block = Block {
+        header: block_header,
+        content: block_content,
+    };
+    block
 }
