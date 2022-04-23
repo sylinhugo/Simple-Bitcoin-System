52f80b52e3d02e2aefb19e07a92b957361b6195d
Hugo Lin <sylin.hugo@gmail.com>
2022-04-23 13:50:19 -0500

    finished project2

--- FinalProject/Part2/contracts/Mint.sol
+++ FinalProject/Part2/contracts/Mint.sol
@@ -93,37 +93,151 @@ contract Mint is Ownable, IMint {
     }
 
     /* TODO: implement your functions here */
-    function registerAsset(
-        address assetToken,
-        uint256 minCollateralRatio,
-        address priceFeed
-    ) external override {}
 
     function openPosition(
         uint256 collateralAmount,
         address assetToken,
         uint256 collateralRatio
-    ) external override {}
+    ) external override {
+        // Make sure we obey the rules
+        require(checkRegistered(assetToken), "Make sure asset is registered");
+        require(
+            collateralRatio >= _assetMap[assetToken].minCollateralRatio,
+            "MCR is too low"
+        );
 
-    function closePosition(uint256 positionIndex) external override {}
+        // transferring collateralAmount EUSD tokens from the message sender to the contract.
+        EUSD(collateralToken).transferFrom(
+            msg.sender,
+            address(this),
+            collateralAmount
+        );
+
+        // calculate the number of minted tokens to send to the message sender.
+        uint256 newAssetAmount = getMintAmount(
+            collateralAmount,
+            assetToken,
+            collateralRatio
+        );
+        address assetAddr = _assetMap[assetToken].token;
+        sAsset(assetAddr).mint(msg.sender, newAssetAmount);
+
+        _idxPositionMap[_currentPositionIndex] = Position(
+            _currentPositionIndex,
+            msg.sender,
+            collateralAmount,
+            assetToken,
+            newAssetAmount
+        );
+        _currentPositionIndex += 1;
+    }
+
+    function closePosition(uint256 positionIndex) external override {
+        // Make sure we obey the rules
+        require(
+            msg.sender == _idxPositionMap[positionIndex].owner,
+            "Only the owner can call this function"
+        );
+
+        // Transfer EUSD tokens locked in the position to the message sender.
+        EUSD(collateralToken).transfer(
+            msg.sender,
+            _idxPositionMap[positionIndex].collateralAmount
+        );
+
+        // Transfer the sAsset tokens from the message sender to the contract and burn these tokens.
+        sAsset(_idxPositionMap[positionIndex].assetToken).burn(
+            _idxPositionMap[positionIndex].owner,
+            _idxPositionMap[positionIndex].assetAmount
+        );
+
+        // delete the position at the given index.
+        delete _idxPositionMap[positionIndex];
+    }
 
     function deposit(uint256 positionIndex, uint256 collateralAmount)
         external
         override
-    {}
+    {
+        // Make sure we obey the rules
+        require(
+            msg.sender == _idxPositionMap[positionIndex].owner,
+            "Make sure, only the owner can deposit a collateral"
+        );
+
+        // transfer deposited tokens from the sender to the contract
+        EUSD(collateralToken).transferFrom(
+            msg.sender,
+            address(this),
+            collateralAmount
+        );
+
+        //  Add collateral amount of the position at the given index
+        _idxPositionMap[positionIndex].collateralAmount += collateralAmount;
+    }
 
     function withdraw(uint256 positionIndex, uint256 withdrawAmount)
         external
         override
-    {}
+    {
+        // Make sure we obey the rules
+        require(
+            msg.sender == _idxPositionMap[positionIndex].owner,
+            "Make sure, only the owner can withdraw a collateral"
+        );
+        require(
+            (_idxPositionMap[positionIndex].collateralAmount - withdrawAmount) /
+                _idxPositionMap[positionIndex].assetAmount >=
+                _assetMap[_idxPositionMap[positionIndex].assetToken]
+                    .minCollateralRatio,
+            "MCR is too low"
+        );
+        require(
+            _idxPositionMap[positionIndex].collateralAmount >= withdrawAmount,
+            "Not enough money"
+        );
 
-    function mint(uint256 positionIndex, uint256 mintAmount)
-        external
-        override
-    {}
+        // Withdraw collateral tokens from the position at the given index
+        // Transfer withdrawn tokens from the contract to the sender
+        EUSD(collateralToken).transfer(msg.sender, withdrawAmount);
 
-    function burn(uint256 positionIndex, uint256 burnAmount)
-        external
-        override
-    {}
+        _idxPositionMap[positionIndex].collateralAmount -= withdrawAmount;
+    }
+
+    function mint(uint256 positionIndex, uint256 mintAmount) external override {
+        // Make sure we obey the rules
+        require(
+            msg.sender == _idxPositionMap[positionIndex].owner,
+            "Make sure, only the owner can mint a given token"
+        );
+        require(
+            _idxPositionMap[positionIndex].collateralAmount /
+                (_idxPositionMap[positionIndex].assetAmount + mintAmount) >=
+                _assetMap[_idxPositionMap[positionIndex].assetToken]
+                    .minCollateralRatio,
+            "MCR is too low"
+        );
+
+        sAsset(_idxPositionMap[positionIndex].assetToken).mint(
+            msg.sender,
+            mintAmount
+        );
+
+        _idxPositionMap[positionIndex].assetAmount += mintAmount;
+    }
+
+    function burn(uint256 positionIndex, uint256 burnAmount) external override {
+        // Make sure we obey the rules
+        require(
+            msg.sender == _idxPositionMap[positionIndex].owner,
+            "Make sure, only the owner can burn a given token"
+        );
+
+        sAsset(_idxPositionMap[positionIndex].assetToken).burn(
+            msg.sender,
+            burnAmount
+        );
+
+        _idxPositionMap[positionIndex].assetAmount -= burnAmount;
+    }
 }
